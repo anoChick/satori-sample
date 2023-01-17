ddacd24e3cf5b0650de79eb64cf400e29b22ffab
anoChick <anoinchick@gmail.com>
2023-01-18 03:03:32 +0900

    あ

---
+++ pages/api/font.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import subsetFont from "subset-font";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const fontBufferArray = await fetch(
+    "http://localhost:3000/DotGothic16-Regular.ttf"
+  ).then((res) => res.arrayBuffer());
+  const fontBuffer = Buffer.from(fontBufferArray);
+  res.setHeader("content-type", "font/ttf");
+  const subsetBuffer = await subsetFont(fontBuffer, "あのイヴォ", {
+    targetFormat: "truetype",
+  });
+
+  res.status(200).send(subsetBuffer);
+}
