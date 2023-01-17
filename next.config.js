b84aa5ce1a64cb243533adb20ca99a541444a40d
anoChick <anoinchick@gmail.com>
2023-01-18 05:43:54 +0900

    a

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      syncWebAssembly: true,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    config.output.webassemblyModuleFilename =
-      (isServer ? "../" : "") + "static/wasm/[modulehash].wasm";
-    return config;
-  },
 };
 
 module.exports = nextConfig;
--- pages/api/satori.tsx
+++ pages/api/satori.tsx
@@ -1,24 +1,41 @@
 import satori from "satori";
 import type { NextRequest } from "next/server";
+import { Resvg } from "@resvg/resvg-js";
+
 export const config = {
+  // Edge Functionsとしてデプロイするための設定
   runtime: "edge",
 };
 
+// フォントファイルURL
 const FONT_URL =
   (process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000") + "/DotGothic16-Regular.ttf";
 
 export default async function handler(req: NextRequest) {
+  // フォントデータのダウンロード
   const fontBufferArray = await fetch(FONT_URL).then((r) => r.arrayBuffer());
 
   const { searchParams } = new URL(req.url);
-  const text = `${searchParams.get("text") ?? "noname"}`;
+  const text = `${searchParams.get("text")}`;
   const svg = await satori(
-    <div style={{ display: "flex", color: "black" }}>hello, {text}</div>,
+    <div
+      style={{
+        display: "flex",
+        width: "300px",
+        height: "300px",
+        color: "white",
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {text}
+    </div>,
     {
-      width: 600,
-      height: 400,
+      width: 300,
+      height: 300,
       fonts: [
         {
           name: "Noto",
@@ -30,10 +47,21 @@ export default async function handler(req: NextRequest) {
     }
   );
 
-  return new Response(svg, {
+  // SVGからPNGに変換する処理
+  const opts = {
+    fitTo: {
+      mode: "width" as const,
+      value: 300,
+    },
+  };
+  const resvg = new Resvg(svg, opts);
+  const pngData = resvg.render();
+  const pngBuffer = pngData.asPng();
+
+  return new Response(pngBuffer, {
     status: 200,
     headers: {
-      "content-type": "image/svg+xml",
+      "content-type": "image/png",
     },
   });
 }
